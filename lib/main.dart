0cc676e441d29f740b12c7e3d6dd8efd28c0a3fa
islom <islomjon20010930@gmail.com>
2023-10-31 21:30:24 +0500

    folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:pizza/business_logic/bloc/order_bloc.dart';
 import 'package:pizza/business_logic/bloc/state_bloc.dart';
 import 'package:pizza/business_logic/cubit/tab_cubit.dart';
+import 'package:pizza/firebase_options.dart';
 import 'package:pizza/presentation/ui/splash_screen/splash_screen.dart';
 import 'package:pizza/presentation/ui/tab_box/tab_box.dart';
 // ignore: depend_on_referenced_packages
@@ -11,7 +13,11 @@ import 'package:provider/provider.dart';
 
 import 'data/database/orders_database.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
   Fluttertoast.showToast(msg: "App initialized");
   await OrderDatabase.instance.initDatabase();
